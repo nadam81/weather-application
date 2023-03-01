98efbfdd2c3cd333c4204076d13447abb709a728
nadam81 <126587122+nadam81@users.noreply.github.com>
2023-03-01 15:25:15 +0100

    weather descrption and weather icon added for Today forecast

--- src/app.js
+++ src/app.js
@@ -16,24 +16,26 @@ let months = [
   "Nov.",
   "Dec.",
 ];
-let month = months[now.getMonth()];
-let hour = now.getHours();
-let minute = now.getMinutes();
-let apiKey = "2a2eaa51d996796495bf456e5b58adf4";
-let fullHour = hour + ":" + minute;
-let tempNumber = document.querySelector(".tempNumber");
-let todayDay = document.querySelector("#today-day");
+let apiKey = "aed005a0bfto11950d353cd8148e9ec7";
+let btnCurrent = document.querySelector("#btn-current");
+let cityForm = document.querySelector("#city-search");
 let cityInput = document.querySelector("#city-input");
 let cityResult = document.querySelector(".cityResult");
-let btnCurrent = document.querySelector("#btn-current");
-let degreeF = document.querySelector("#degreeF");
 let degreeC = document.querySelector("#degreeC");
-let cityForm = document.querySelector("#city-search");
-let paris = document.querySelector("#paris");
+let degreeF = document.querySelector("#degreeF");
+let hour = now.getHours();
+let humidity = document.querySelector("#humidity-value");
 let london = document.querySelector("#london");
 let madrid = document.querySelector("#madrid");
+let minute = now.getMinutes();
+let fullHour = hour + ":" + minute;
+let month = months[now.getMonth()];
+let paris = document.querySelector("#paris");
+let tempNumber = document.querySelector(".tempNumber");
+let todayDay = document.querySelector("#today-day");
+let weatherDescription = document.querySelector("#weather-description");
 let wind = document.querySelector("#wind-value");
-let humidity = document.querySelector("#humidity-value");
+let weatherIconToday = document.querySelector("#weather-icon-today");
 
 // Mise en forme date
 function todayDate(day, date, month) {
@@ -61,58 +63,57 @@ getPosition();
 // Pour les résultats de Paris
 function showParis() {
   let city = "Paris";
-  let cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let cityUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(cityUrl).then(showWeather);
   axios.get(cityUrl).then(showCityName);
 }
-paris.addEventListener("click", showParis);
 
 // Pour les résultats de London
 function showLondon(event) {
   let city = "London";
-  let cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let cityUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(cityUrl).then(showWeather);
   axios.get(cityUrl).then(showCityName);
 }
-london.addEventListener("click", showLondon);
 
 // Pour les résultats de Madrid
 function showMadrid() {
   let city = "Madrid";
-  let cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let cityUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(cityUrl).then(showWeather);
   axios.get(cityUrl).then(showCityName);
 }
-madrid.addEventListener("click", showMadrid);
 
 // Pour les résultats de search
 function showSearch(event) {
   event.preventDefault();
   let city = cityInput.value;
   city = city.trim();
-  let cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let cityUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(cityUrl).then(showWeather);
   axios.get(cityUrl).then(showCityName);
 }
-cityForm.addEventListener("submit", showSearch);
 
 // bouton Current ou landing page
 function currentPosition(response) {
-  let currentLongitude = response.coords.longitude;
-  let currentLatitude = response.coords.latitude;
-  let currentUrlTemp = `https://api.openweathermap.org/data/2.5/weather?lat=${currentLatitude}&lon=${currentLongitude}&appid=${apiKey}&units=metric`;
+  let lon = response.coords.longitude;
+  let lat = response.coords.latitude;
+  let currentUrlTemp = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
   axios.get(currentUrlTemp).then(showWeather);
-  let currentCityNameUrl = `https://api.openweathermap.org/geo/1.0/reverse?lat=${currentLatitude}&lon=${currentLongitude}&appid=${apiKey}`;
-  axios.get(currentCityNameUrl).then(showCurrentCityName);
+  axios.get(currentUrlTemp).then(showCityName);
 }
 
 // Show the current Temp both in Cº and Fº
 function showWeather(response) {
-  let cityCurrentTemp = Math.round(response.data.main.temp);
+  console.log(response);
+  console.log(response.data.condition.icon_url);
+  let cityCurrentTemp = Math.round(response.data.temperature.current);
   tempNumber.innerHTML = cityCurrentTemp;
   let tempNumberF = (cityCurrentTemp * 9) / 5 + 32;
   wind.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
-  humidity.innerHTML = `${Math.round(response.data.main.humidity)} %`;
+  humidity.innerHTML = `${Math.round(response.data.temperature.humidity)} %`;
+  weatherDescription.innerHTML = response.data.condition.description;
+  weatherIconToday.src = response.data.condition.icon_url;
 
   // degree in Fº
   function showDegreeF(event) {
@@ -140,8 +141,20 @@ function showCurrentCityName(response) {
 
 // CityName for Search and listed countries
 function showCityName(response) {
-  cityResult.innerHTML = response.data.name;
+  cityResult.innerHTML = response.data.city;
 }
+
+// When click on Paris
+paris.addEventListener("click", showParis);
+
+// When click on London
+london.addEventListener("click", showLondon);
+
+// When click on Madrid
+madrid.addEventListener("click", showMadrid);
 
 // When click on Current Button
 btnCurrent.addEventListener("click", getPosition);
+
+// When click on Search
+cityForm.addEventListener("submit", showSearch);

fed19cb3c73d97cfa42af21991225b200aea47ca
nadam81 <126587122+nadam81@users.noreply.github.com>
2023-03-01 11:51:26 +0100

    General clean up

--- src/app.js
+++ src/app.js
@@ -52,21 +52,27 @@ function todayHours(fullHour) {
 }
 todayHours(fullHour);
 
+// Allows the page do download with current position information
+function getPosition() {
+  navigator.geolocation.getCurrentPosition(currentPosition);
+}
+getPosition();
+
 // Pour les résultats de Paris
 function showParis() {
   let city = "Paris";
   let cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  cityResult.innerHTML = city;
   axios.get(cityUrl).then(showWeather);
+  axios.get(cityUrl).then(showCityName);
 }
 paris.addEventListener("click", showParis);
 
-// Pour les résultats de Paris
-function showLondon() {
+// Pour les résultats de London
+function showLondon(event) {
   let city = "London";
   let cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  cityResult.innerHTML = city;
   axios.get(cityUrl).then(showWeather);
+  axios.get(cityUrl).then(showCityName);
 }
 london.addEventListener("click", showLondon);
 
@@ -74,50 +80,38 @@ london.addEventListener("click", showLondon);
 function showMadrid() {
   let city = "Madrid";
   let cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  cityResult.innerHTML = city;
   axios.get(cityUrl).then(showWeather);
+  axios.get(cityUrl).then(showCityName);
 }
 madrid.addEventListener("click", showMadrid);
 
-// city search, trimed, cappitalised and the API URL
-function showCity(event) {
+// Pour les résultats de search
+function showSearch(event) {
   event.preventDefault();
-  let citySearch = cityInput.value;
-  citySearch = citySearch.trim();
-  citySearch =
-    citySearch.charAt(0).toUpperCase() + citySearch.slice(1).toLowerCase();
-  let searchUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}&units=metric`;
-  cityResult.innerHTML = citySearch;
-  axios.get(searchUrl).then(showWeather);
+  let city = cityInput.value;
+  city = city.trim();
+  let cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(cityUrl).then(showWeather);
+  axios.get(cityUrl).then(showCityName);
 }
-cityForm.addEventListener("submit", showCity);
+cityForm.addEventListener("submit", showSearch);
 
 // bouton Current ou landing page
 function currentPosition(response) {
-  console.log(response);
   let currentLongitude = response.coords.longitude;
   let currentLatitude = response.coords.latitude;
-  console.log(currentLatitude);
   let currentUrlTemp = `https://api.openweathermap.org/data/2.5/weather?lat=${currentLatitude}&lon=${currentLongitude}&appid=${apiKey}&units=metric`;
-  let currentUrlCityName = `http://api.openweathermap.org/geo/1.0/reverse?lat=${currentLatitude}&lon=${currentLongitude}&appid=${apiKey}`;
   axios.get(currentUrlTemp).then(showWeather);
-  axios.get(currentUrlCityName).then(showCurrentCityName);
-
-  // Show the current City Name
-  function showCurrentCityName(response) {
-    console.log(response.data[0].name);
-    cityResult.innerHTML = response.data[0].name;
-  }
+  let currentCityNameUrl = `http://api.openweathermap.org/geo/1.0/reverse?lat=${currentLatitude}&lon=${currentLongitude}&appid=${apiKey}`;
+  axios.get(currentCityNameUrl).then(showCurrentCityName);
 }
 
 // Show the current Temp both in Cº and Fº
 function showWeather(response) {
-  console.log(response);
   let cityCurrentTemp = Math.round(response.data.main.temp);
   tempNumber.innerHTML = cityCurrentTemp;
   let tempNumberF = (cityCurrentTemp * 9) / 5 + 32;
   wind.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
-  console.log(wind);
   humidity.innerHTML = `${Math.round(response.data.main.humidity)} %`;
 
   // degree in Fº
@@ -139,13 +133,15 @@ function showWeather(response) {
   degreeC.addEventListener("click", showDegreeC);
 }
 
-// Precipitation
-
-// Allows the page do download with current position information
-function getPosition() {
-  navigator.geolocation.getCurrentPosition(currentPosition);
+// cityName for landing and current button
+function showCurrentCityName(response) {
+  cityResult.innerHTML = response.data[0].name;
 }
-getPosition();
+
+// CityName for Search and listed countries
+function showCityName(response) {
+  cityResult.innerHTML = response.data.name;
+}
 
 // When click on Current Button
 btnCurrent.addEventListener("click", getPosition);
